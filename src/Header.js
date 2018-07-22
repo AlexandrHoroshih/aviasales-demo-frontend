e5d75d2a6de305ebd4c6c188aa9949475a371d0f
AlexanderHoroshih <xad71rus@gmail.com>
2018-07-22 21:46:40 +0800

    feat: header is responsive now

--- src/Header.js
+++ src/Header.js
@@ -22,6 +22,7 @@ const Logo = styled.img`
 `;
 
 const Title = styled.h1`
+  margin-top: 81px;
   font-family: Roboto;
   font-style: normal;
   font-weight: bold;
@@ -30,6 +31,15 @@ const Title = styled.h1`
   text-align: center;
 
   color: #ffffff;
+
+  @media (max-width: 576px) {
+    font-size: 20px;
+    margin-top: 47px;
+  }
+
+  @media (min-width: 1200px) {
+    margin-top: 213px;
+  }
 `;
 
 const Tagline = styled.h2`
@@ -41,19 +51,49 @@ const Tagline = styled.h2`
   text-align: center;
 
   color: #ffffff;
+
+  @media (max-width: 576px) {
+    display: none;
+  }
 `;
 
 const SearchForm = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   margin-top: 40px;
   margin-bottom: 122px;
-`;
+  @media (max-width: 576px) {
+    margin-top: 0px;
+    margin-bottom: 88px;
+  }
 
+  @media (min-width: 1200px) {
+    margin-bottom: 254px;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+`;
+const Inputs = styled.div`
+  display: flex;
+  @media (min-width: 1200px) {
+    flex-direction: row;
+  }
+
+  @media (max-width: 1199px) {
+    flex-direction: column;
+  }
+`;
 const FlightInputs = styled.div`
   display: flex;
   flex-direction: row;
+  @media (max-width: 576px) {
+    flex-direction: column;
+  }
   width: 100%;
+  @media (min-width: 1200px) {
+    flex-basis: 45%;
+  }
 `;
 
 const Departure = styled.div`
@@ -61,7 +101,11 @@ const Departure = styled.div`
   display: flex;
   flex-basis: 50%;
   margin-right: 2px;
+  @media (max-width: 576px) {
+    margin-right: 0px;
+  }
 `;
+
 const DepartureInput = styled.input`
   width: 100%;
   border: none;
@@ -78,6 +122,14 @@ const DepartureInput = styled.input`
   color: #4a4a4a;
   &:: placeholder {
     color: #a0b0b9;
+  }
+
+  @media (max-width: 576px) {
+    border-top-right-radius: 4px;
+  }
+
+  @media (min-width: 1200px) {
+    border-bottom-left-radius: 4px;
   }
 `;
 
@@ -109,11 +161,23 @@ const ReverseButtonIcon = styled.img``;
 const Destination = styled.div`
   display: flex;
   flex-basis: 50%;
+  @media (min-width: 1200px) {
+    margin-right: 2px;
+  }
 `;
 
 const DestinationInput = DepartureInput.extend`
   border-top-right-radius: 4px;
   border-top-left-radius: 0px;
+  @media (max-width: 576px) {
+    border-top-right-radius: 0px;
+    margin-top: 2px;
+  }
+
+  @media (min-width: 1200px) {
+    border-top-right-radius: 0;
+    border-bottom-left-radius: 0;
+  }
 `;
 
 const DetailsInputs = styled.div`
@@ -121,29 +185,68 @@ const DetailsInputs = styled.div`
   flex-direction: row;
   width: 100%;
   margin-top: 2px;
+  @media (max-width: 576px) {
+    flex-wrap: wrap;
+    margin-top: 1px;
+  }
+
+  @media (min-width: 1200px) {
+    flex-basis: 55%;
+    margin-top: 0px;
+  }
+`;
+
+const Dates = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-basis: 50%;
+  flex-wrap: nowrap;
+  margin-right: 2px;
+  @media (max-width: 576px) {
+    flex-basis: 100%;
+    margin-right: 0px;
+  }
 `;
 
 const DateTo = styled.div`
   display: flex;
   position: relative;
-  flex-basis: 25%;
+  flex-basis: 50%;
   margin-right: 2px;
+  @media (max-width: 576px) {
+    margin-right: 0px;
+    padding-right: 2px;
+  }
 `;
 
 const DateToInput = DepartureInput.extend`
   border-top-left-radius: 0px;
   border-bottom-left-radius: 4px;
+  @media (max-width: 576px) {
+    border-bottom-left-radius: 0px;
+    border-top-right-radius: 0px;
+  }
+
+  @media (min-width: 1200px) {
+    border-bottom-left-radius: 0px;
+  }
 `;
 
 const DateBack = styled.div`
   display: flex;
   position: relative;
-  flex-basis: 25%;
+  flex-basis: 50%;
 `;
 
 const DateBackInput = DepartureInput.extend`
   border-top-left-radius: 0px;
-  margin-right: 2px;
+  @media (max-width: 576px) {
+    border-top-right-radius: 0px;
+    border-bottom-left-radius: 0;
+  }
+  @media (min-width: 1200px) {
+    border-bottom-left-radius: 0;
+  }
 `;
 
 const CalendarButton = styled.button`
@@ -159,16 +262,31 @@ const CalendarIcon = styled.img``;
 const PassengerInfo = styled.div`
   display: flex;
   flex-basis: 50%;
+
+  @media (max-width: 576px) {
+    flex-basis: 100%;
+  }
 `;
 
 const FakeInput = DepartureInput.extend`
   border-top-left-radius: 0px;
   border-bottom-right-radius: 4px;
+  @media (max-width: 576px) {
+    border-bottom-left-radius: 4px;
+    margin-top: 2px;
+    border-top-right-radius: 0px;
+  }
+
+  @media (min-width: 1200px) {
+    border-top-right-radius: 4px;
+    border-bottom-left-radius: 0;
+  }
 `;
 
 const SearchButton = styled.button`
   display: flex;
   align-self: center;
+  justify-content: center;
   margin-top: 32px;
   border: none;
   background: #ff9241;
@@ -185,6 +303,15 @@ const SearchButton = styled.button`
   font-size: 28px;
 
   color: #ffffff;
+
+  @media (min-width: 1200px) {
+    margin-top: 48px;
+  }
+
+  @media (max-width: 576px) {
+    margin-top: 16px;
+    width: 100%;
+  }
 `;
 
 const Plane = styled.img`
@@ -203,38 +330,51 @@ export default function() {
         <div className="row center-xs">
           <div className="col-xs-12 col-md-10">
             <SearchForm>
-              <FlightInputs>
-                <Departure>
-                  <DepartureInput placeholder="Город вылета" type="text" />
-                  <IATA>mow</IATA>
-                  <ReverseButton>
-                    <ReverseButtonIcon
-                      src={reverse}
-                      alt="reverse departure and destination points"
+              <Inputs>
+                <FlightInputs>
+                  <Departure>
+                    <DepartureInput placeholder="Город вылета" type="text" />
+                    <IATA>mow</IATA>
+                    <ReverseButton>
+                      <ReverseButtonIcon
+                        src={reverse}
+                        alt="reverse departure and destination points"
+                      />
+                    </ReverseButton>
+                  </Departure>
+                  <Destination>
+                    <DestinationInput
+                      placeholder="Город прибытия"
+                      type="text"
                     />
-                  </ReverseButton>
-                </Departure>
-                <Destination>
-                  <DestinationInput placeholder="Город прибытия" />
-                </Destination>
-              </FlightInputs>
-              <DetailsInputs>
-                <DateTo>
-                  <DateToInput placeholder="Туда" />
-                  <CalendarButton>
-                    <CalendarIcon src={calendar} alt="calendar date picker" />{" "}
-                  </CalendarButton>
-                </DateTo>
-                <DateBack>
-                  <DateBackInput placeholder="Обратно" />
-                  <CalendarButton>
-                    <CalendarIcon src={calendar} alt="calendar date picker" />{" "}
-                  </CalendarButton>
-                </DateBack>
-                <PassengerInfo>
-                  <FakeInput placeholder="1 пассажир" />
-                </PassengerInfo>
-              </DetailsInputs>
+                  </Destination>
+                </FlightInputs>
+                <DetailsInputs>
+                  <Dates>
+                    <DateTo>
+                      <DateToInput placeholder="Туда" type="text" />
+                      <CalendarButton>
+                        <CalendarIcon
+                          src={calendar}
+                          alt="calendar date picker"
+                        />
+                      </CalendarButton>
+                    </DateTo>
+                    <DateBack>
+                      <DateBackInput placeholder="Обратно" type="text" />
+                      <CalendarButton>
+                        <CalendarIcon
+                          src={calendar}
+                          alt="calendar date picker"
+                        />
+                      </CalendarButton>
+                    </DateBack>
+                  </Dates>
+                  <PassengerInfo>
+                    <FakeInput placeholder="1 пассажир" type="text" />
+                  </PassengerInfo>
+                </DetailsInputs>
+              </Inputs>
               <SearchButton>
                 Найти билеты<Plane src={aero} alt="Search" />
               </SearchButton>
