9e696c57aea4c1fa5621cffdb8e0fcc25896f201
Alexander Khoroshikh <xad71rus@gmail.com>
2018-07-26 02:58:55 +0800

    feat: routing now works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Destinations from "./Destinations";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const Main = () => (
+  <div>
+    <Header />
+    <Destinations />
+  </div>
+);
+
+const Search = () => (
+  <div>
+    <h2>content</h2>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Destinations />
+        <Router>
+          <div>
+            <Route exact path="/" component={Main} />
+            <Route path="/Search" component={Search} />
+          </div>
+        </Router>
       </div>
     );
   }
