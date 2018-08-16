bd023f44d448cef3df889d0fc73fc974ec412938
Alexander Khoroshikh <xad71rus@gmail.com>
2018-08-16 11:28:06 +0800

    feat: filters visibility fix

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -20,7 +20,7 @@ const FlitersWrapper = styled.div`
   flex-flow: column nowrap;
   flex-basis: 25%;
   margin-right: 16px;
-  @media (max-width: 1024px;) {
+  @media (max-width: 1024px) {
     display: none;
   }
 `;
